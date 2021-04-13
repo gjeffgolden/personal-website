48bfdb792641de8d059780e6921b37afc8e9ce04
gjeffgolden <gjgolden08@gmail.com>
2021-04-13 14:26:55 -0600

    Contact Button.

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Avatar, Link } from '@material-ui/core'
+import { Container, Avatar, Link, Button } from '@material-ui/core'
 import Background from './assets/personal-site-hero.jpg';
 import Headshot from './assets/golden-headshot.JPG'
 import LinkedIn from './assets/icons/linkedin-icon.jpg'
@@ -12,18 +12,23 @@ export default function Header() {
         <Container maxWidth="xl" style={{height: '62.5rem', backgroundImage: `url(${Background})`}}>
             <Container maxWidth="m" style={{paddingTop: '2rem', display: 'flex', flexFlow: 'row', alignItems: 'flex-end'}}>
                 <Avatar alt="Jeff Golden" src={Headshot} style={{height: '10rem', width: '10rem'}} />
-                <Link href="https://www.linkedin.com/in/gjeffgolden/" target="_blank">
-                    <Avatar alt="LinkedIn Icon" src={LinkedIn}/>
-                </Link>
-                <Link href="https://github.com/gjeffgolden" target="_blank">
-                    <Avatar alt="GitHub Icon" src={GitHub}/>
-                </Link>
-                <Link href="https://www.instagram.com/gjeffgolden/" target="_blank">
-                    <Avatar alt="Instagram Icon" src={Instagram}/>
-                </Link>
-                <Link href="https://www.twitter.com/gjeffgolden/" target="_blank">
-                    <Avatar alt="Twitter Icon" src={Twitter}/>
-                </Link>
+                <Container maxWidth="sm" style={{display: 'flex', flexFlow: 'column', margin: '0'}}>
+                    <Button fullWidth={false} size='small' variant='contained' style={{width: '10rem', marginBottom: '0.25rem'}}>Contact</Button>
+                    <Container maxWidth="sm" disableGutters={true} style={{display: 'flex', flexFlow: 'row'}}>
+                        <Link href="https://www.linkedin.com/in/gjeffgolden/" target="_blank">
+                            <Avatar alt="LinkedIn Icon" src={LinkedIn}/>
+                        </Link>
+                        <Link href="https://github.com/gjeffgolden" target="_blank">
+                            <Avatar alt="GitHub Icon" src={GitHub}/>
+                        </Link>
+                        <Link href="https://www.instagram.com/gjeffgolden/" target="_blank">
+                            <Avatar alt="Instagram Icon" src={Instagram}/>
+                        </Link>
+                        <Link href="https://www.twitter.com/gjeffgolden/" target="_blank">
+                            <Avatar alt="Twitter Icon" src={Twitter}/>
+                        </Link>
+                    </Container>
+                </Container>
             </Container>
         </Container>
     )
