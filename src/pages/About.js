2c12ccc77ef9e00610fb26b48cfcad5a46f78ad1
gjeffgolden <gjgolden08@gmail.com>
2021-04-14 13:59:09 -0600

    Added cards to Projects page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Container } from '@material-ui/core'
+import { Container, Card, Typography, CardMedia } from '@material-ui/core'
 
 export default function About() {
     return (
-        <Container maxWidth='xl' style={{height: "60%", width: "80%", backgroundColor: "black", marginTop: '3rem', opacity: "0.3"}}>
-            
+        <Container 
+            maxWidth='xl' 
+            style={{height: "60%", width: "80%", backgroundColor: "rgb(0, 0, 0, .3", marginTop: '3rem', display: 'flex', flexFlow: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}
+        >
+            <Container>
+                <Typography variant="h1">Hi. I'm Jeff.</Typography>
+            </Container>
+            <Card style={{height: "90%", width: "40%"}}>
+                <CardMedia />
+            </Card>
         </Container>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Container } from '@material-ui/core'
+import { Container, Card } from '@material-ui/core'
 
 export default function Projects() {
     return (
-        <Container maxWidth='xl' style={{height: "60%", width: "80%", backgroundColor: "black", marginTop: '3rem', opacity: "0.3"}}>
-            
+        <Container 
+            maxWidth='xl' 
+            style={{height: "60%", width: "80%", backgroundColor: "rgb(0, 0, 0, .3", marginTop: '3rem', display: 'flex', flexFlow: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}
+        >
+            <Card style={{height: "90%", width: "32%"}}></Card>
+            <Card style={{height: "90%", width: "32%"}}></Card>
+            <Card style={{height: "90%", width: "32%"}}></Card>
         </Container>
     )
 }
