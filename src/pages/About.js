88afd1609d7b408957dd2ce1f86dc1c802dac7e0
gjeffgolden <gjgolden08@gmail.com>
2021-04-14 15:52:58 -0600

    Added icons, text, styling to About page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import { Container, Card, Typography, CardMedia } from '@material-ui/core'
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import SearchIcon from '@material-ui/icons/Search';
+import WebIcon from '@material-ui/icons/Web';
+import FilterHdrIcon from '@material-ui/icons/FilterHdr';
 
 export default function About() {
     return (
         <Container 
             maxWidth='xl' 
-            style={{height: "60%", width: "80%", backgroundColor: "rgb(0, 0, 0, .1", marginTop: '3rem', display: 'flex', flexFlow: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}
+            style={{height: "60%", width: "80%", minHeight: 'fit-content', backgroundColor: "rgb(0, 0, 0, .1", marginTop: '3rem', display: 'flex', flexFlow: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}
         >
-            <Container maxWidth="lg" style={{textAlign: "left", height: "95%", display: 'flex', flexFlow: 'column'}}>
-                <Typography variant="h1">Hi. I'm Jeff.</Typography>
-                <Typography variant="h4">Hi. I'm Jeff.</Typography>
-                <Typography variant="h4">Hi. I'm Jeff.</Typography>
-                
+            <Container maxWidth="lg" style={{textAlign: "left", height: "fit-content", display: 'flex', flexFlow: 'column'}}>
+                <Typography variant="h1" gutterBottom={true}>Hi. I'm Jeff.</Typography>
+                <Container maxWidth="lg" disableGutters={true} style={{backgroundColor: "rgb(0, 0, 0, .6", paddingLeft: "1rem"}}>
+                    <Typography gutterBottom={true} variant="h5" style={{color: "white"}}>
+                        <DoneAllIcon style={{marginRight: "1rem", fontSize: '3rem'}} />   
+                        React.js, Redux.js, JavaScript, TypeScript, Vue.js, Ruby on Rails, Django, GitHub, Firebase
+                    </Typography>
+                    <Typography gutterBottom={true} variant="h5" style={{color: "white"}}>
+                        <EmojiPeopleIcon style={{marginRight: "1rem", fontSize: "3rem"}} />   
+                        Proven professional, teammate and leader across multiple industries
+                    </Typography>
+                    <Typography gutterBottom={true} variant="h5" style={{color: "white"}}>
+                        <SearchIcon style={{marginRight: "1rem", fontSize: "3rem"}} />   
+                        Former writer and editor with a passion for clean, readable, typo-free code
+                    </Typography>
+                    <Typography gutterBottom={true} variant="h5" style={{color: "white"}}>
+                        <WebIcon style={{marginRight: "1rem", fontSize: "3rem"}} />   
+                        Digital marketing expert with web design and content management experience
+                    </Typography>
+                    <Typography gutterBottom={true} variant="h5" style={{color: "white"}}>
+                        <FilterHdrIcon style={{marginRight: "1rem", fontSize: "3rem"}} />   
+                        Avid mountaineer, backpacker, conservationist and 14er Finisher
+                    </Typography>
+                </Container>
             </Container>
             <Card style={{height: "90%", width: "40%"}}>
                 <CardMedia
